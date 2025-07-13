b6ceb4b5c82622ee7c0540fe24915801c034eec0
mjnji-kim0524 <always0524@naver.com>
2025-07-13 23:18:57 +0900

    [5주차 과제-계산기]
    main.js파일 리팩토링
    
    - 연산자별 처리결과 switch문 적용

--- src/week5/03.calculator/begin/scripts/main.js
+++ src/week5/03.calculator/begin/scripts/main.js
@@ -1,7 +1,6 @@
 {
   const keys = document.querySelector('.calculator__keys')
   const display = document.querySelector('.calculator__display')
-  
 
   let preResult = ''
   let operator = ''
@@ -11,7 +10,6 @@
     const result = display.textContent
     const keyType = button.dataset.key
     const buttonType = button.dataset.buttonType
-    operator = keyType
 
     // 숫자버튼 처리
     if (buttonType === 'number') {
@@ -22,24 +20,9 @@
       }
     } else if(buttonType === 'operator') {
       preResult = result
-      display.textContent = preResult
       operator = keyType
-    } else {
-      // 연산자 외 (소수점, 등호, AC) 처리
-      // switch(keyType) {
-      //   case 'plus' : 
-      //   display.textContent = parseFloat(preResult) + parseFloat(newResult)
-      //   break
-      //   case 'minus' : 
-      //   display.textContent = parseFloat(preResult) - parseFloat(newResult)
-      //   break
-      //   case 'times' : 
-      //   display.textContent = parseFloat(preResult) * parseFloat(newResult)
-      //   break
-      //   case 'divide' : 
-      //   display.textContent = parseFloat(preResult) / parseFloat(newResult)
-      //   break
-      // }
+      display.textContent = preResult
+    } 
       switch(keyType) {
         case 'decimal' : 
           if (!result.includes('.')) {
@@ -51,13 +34,16 @@
           break
         case 'equal' : 
           // 연산자 처리
-          const newResult = result
-          if(keyType === 'plus') display.textContent = parseFloat(preResult) + parseFloat(newResult)
-          if(keyType === 'minus') display.textContent = parseFloat(preResult) - parseFloat(newResult)
-          if(keyType === 'times') display.textContent = parseFloat(preResult) * parseFloat(newResult)
-          if(keyType === 'divide') display.textContent = parseFloat(preResult) / parseFloat(newResult)
-          break
+          const newResult = display.textContent
+          let finalResult = ''
+          switch(operator) {
+            case 'plus' : finalResult = parseFloat(preResult) + parseFloat(newResult); break
+            case 'minus': finalResult = parseFloat(preResult) - parseFloat(newResult); break
+            case 'times': finalResult = parseFloat(preResult) * parseFloat(newResult); break
+            case 'divide' : finalResult = parseFloat(preResult) / parseFloat(newResult); break
+          }
+          display.textContent = finalResult
       }
-    }
+
   })
 }
