47109836a8e46425b852cdba4bc160f70f45fdbc
MINJI <always0524@naver.com>
2025-12-15 10:16:28 +0900

    refactor. [5주차 과제-계산기] 리팩토링 진행 - 사칙연산 구현 (소수점 제외)

--- src/week5/03.calculator/begin/scripts/main.js
+++ src/week5/03.calculator/begin/scripts/main.js
@@ -1,49 +1,52 @@
 {
-  const keys = document.querySelector('.calculator__keys')
-  const display = document.querySelector('.calculator__display')
+  const keys = document.querySelector(".calculator__keys");
+  const display = document.querySelector(".calculator__display");
 
-  let preResult = ''
-  let operator = ''
+  let preResult = "";
+  let operator = "";
 
-  keys.addEventListener('click', (e) => {
-    const button = e.target.closest('button')
-    const result = display.textContent
-    const keyType = button.dataset.key
-    const buttonType = button.dataset.buttonType
+  keys.addEventListener("click", (e) => {
+    const button = e.target.closest("button");
+    const result = display.textContent;
+    const keyType = button.dataset.key;
+    const buttonType = button.dataset.buttonType;
 
     // 숫자버튼 처리
-    if (buttonType === 'number') {
-      if(result === '0') {
-        display.textContent = button.textContent
-      } else{
-        display.textContent += button.textContent
-      }
-    } else if(buttonType === 'operator') {
-      preResult = result
-      operator = keyType
-      display.textContent = preResult
-    } 
-      switch(keyType) {
-        case 'decimal' : 
-          if (!result.includes('.')) {
-            display.textContent = result + '.'  
-          } 
-          break
-        case 'clear' : 
-          display.textContent = '0'
-          break
-        case 'equal' : 
-          // 연산자 처리
-          const newResult = display.textContent
-          let finalResult = ''
-          switch(operator) {
-            case 'plus' : finalResult = parseFloat(preResult) + parseFloat(newResult); break
-            case 'minus': finalResult = parseFloat(preResult) - parseFloat(newResult); break
-            case 'times': finalResult = parseFloat(preResult) * parseFloat(newResult); break
-            case 'divide' : finalResult = parseFloat(preResult) / parseFloat(newResult); break
-          }
-          display.textContent = finalResult
-      }
-
-  })
+    if (buttonType === "number") {
+      display.textContent = button.textContent;
+    } else if (buttonType === "operator") {
+      preResult = result;
+      operator = keyType;
+      display.textContent = preResult;
+    }
+    switch (keyType) {
+      case "decimal":
+        if (!result.includes(".")) {
+          display.textContent = result + "." + result;
+        }
+        break;
+      case "clear":
+        display.textContent = "0";
+        break;
+      case "equal":
+        // 연산자 처리
+        const newResult = display.textContent;
+        let finalResult = "";
+        switch (operator) {
+          case "plus":
+            finalResult = parseFloat(preResult) + parseFloat(newResult);
+            break;
+          case "minus":
+            finalResult = parseFloat(preResult) - parseFloat(newResult);
+            break;
+          case "times":
+            finalResult = parseFloat(preResult) * parseFloat(newResult);
+            break;
+          case "divide":
+            finalResult = parseFloat(preResult) / parseFloat(newResult);
+            break;
+        }
+        display.textContent = finalResult;
+    }
+  });
 }
